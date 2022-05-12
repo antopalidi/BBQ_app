28bb25dfd40f5e4a2c2232e7b3782f21ea6c3528
annatopalidi <topalidisanna@gmail.com>
2022-05-12 18:07:25 +0200

    add language choice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user_edit?
 
+  around_action :switch_locale
+
+  private
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
       :account_update,
@@ -12,5 +16,22 @@ class ApplicationController < ActionController::Base
 
   def current_user_edit?(event)
     user_signed_in? && event.user == current_user
+  end
+
+  def switch_locale(&action)
+    locale = locale_from_url || I18n.default_locale
+    I18n.with_locale locale, &action
+  end
+
+  def locale_from_url
+    locale = params[:locale]
+
+    return locale if I18n.available_locales.map(&:to_s).include?(locale)
+
+    nil
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users, only: %i[show edit update]
-  resources :events
-  root to: "events#index"
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/  do
+    devise_for :users
+    resources :users, only: %i[show edit update]
+    resources :events
+    root to: "events#index"
+  end
 end
