4649aef7d8b4768efe011a622b524cd045d607ad
annatopalidi <topalidisanna@gmail.com>
2022-05-16 18:40:01 +0200

    disabled sending mail from dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,16 +67,18 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: '587',
-    user_name: ENV["MAILJET_SENDER"],
-    password: ENV["MAIL_PASSWORD"],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  #
+  #
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  #
+  # config.action_mailer.delivery_method = :smtp
+  #
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: '587',
+  #   user_name: ENV["MAILJET_SENDER"],
+  #   password: ENV["MAIL_PASSWORD"],
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true
+  # }
 end
