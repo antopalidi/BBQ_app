68ae48052e5c1455139bc118177673c4edbce014
annatopalidi <topalidisanna@gmail.com>
2022-05-18 08:57:40 +0200

    fix subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -8,19 +8,18 @@ class Subscription < ApplicationRecord
   end
 
   with_options unless: -> { user.present? } do
-    validates :event, presence: true
     validates :user_name, presence: true
-    validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
-    validates :user, uniqueness: { scope: :event_id }
+    validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/,
+              uniqueness: { scope: :event_id }
     validate :cannot_use_someone_email
   end
 
   def user_name
-    user.present? ? user&.name : super
+    user&.name || super
   end
 
   def user_email
-    user.present? ? user&.email : super
+    user&.email || super
   end
 
   private
